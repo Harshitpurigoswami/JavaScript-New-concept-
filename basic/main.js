0b54e432724831f18cdbeaf68a315be33a1c885c
Harshit <hpurigoswami786@gmail.com>
2024-03-18 08:26:33 +0530

    create switch case 1

---
+++ basic/main.js
@@ -0,0 +1,24 @@
+// var a = 'harshit';
+console.log("Harsiht");
+var a = prompt('please enter day');
+
+switch(a){
+    case "sunday" : alert('Your day is ' + a);
+    break;
+    case "monday" : alert('Your day is ' + a);
+    break;
+    case "tuesday" : alert('Your day is ' + a);
+    break;
+    case "wednesday" : alert('Your day is ' + a);
+    break;
+    case "thursday" : alert('Your day is ' + a);
+    break;
+    case "friday" : alert('Your day is ' + a);
+    break;
+    case "saturday" : alert('Your day is ' + a);
+    break;
+    default : alert('default');
+}
+// var heading = document.querySelector(h1);
+// heading.innerHTML.a;
+ 
