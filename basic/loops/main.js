979d00804bf60c43ddc5e9230d491d1a1b3fe88e
Harshit <hpurigoswami786@gmail.com>
2024-05-08 00:21:52 +0530

    practice of loops

---
+++ basic/loops/main.js
@@ -0,0 +1,18 @@
+var a = 1;
+
+// while 
+// while(a < 5){
+//     console.log( a +' harshit');
+//     a++;
+// }
+
+// Do while
+// do{
+//     console.log(a + " harshit");
+//     a++;
+// }while(a < 5)
+
+// for
+for(let b = 1; b < 5 ; b++){
+    console.log(b + " Harsiht");
+}
