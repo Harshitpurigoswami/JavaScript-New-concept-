417e6b9a7ee215c22668c7f5eb0a85d184eff2d1
Harshit <hpurigoswami786@gmail.com>
2024-05-08 08:04:18 +0530

    working on function

---
+++ basic/function/main.js
@@ -0,0 +1,9 @@
+function newFun(a,b){
+    var c = a + b;
+    // alert('thanks for click button');
+    alert(a + " + " + b + " = " + c);
+    return c;
+}
+
+var newFunValue = newFun(2, 2);
+console.log(newFunValue);
