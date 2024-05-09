4d3c5230b74dca2cedb1dd47f75a26dfa179ed79
Harshit <hpurigoswami786@gmail.com>
2024-05-10 03:28:56 +0530

    clear concept of var let behaviour in settimeout

--- basic/hoisting.js
+++ basic/hoisting.js
@@ -1,10 +1,21 @@
 
-let x = 1; 
-// console.log(x); 
-function hello(){
-    x = 100;
-    // console.log(x); 
-    var a = 10;
+var x = 1;  
+if(true){
+    var x = 100;
+    setTimeout(()=>{
+        console.log(x); 
+    },0);
+}  
+
+if(true){
+    var x = 200;
+    setTimeout(()=>{
+        console.log(x); 
+    },0);
 } 
-hello();
-console.log(a); 
+
+
+// in the case of var 
+// settimeout excute the line in last and he want a value of x and x is global initials so last value is get 
+// and let is block scope so when settime out is come back he want x value so he give to the value on the block 
+// scope basis give so inside a block scope value .
