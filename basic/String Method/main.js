838b75a5fa9d4a20502b3fc3f90df98002ea83bb
Harshit <hpurigoswami786@gmail.com>
2024-05-14 07:39:12 +0530

    string and array method are doing

---
+++ basic/String Method/main.js
@@ -0,0 +1,67 @@
+// String
+/*
+    concat               startsWith
+    endsWith             includes              
+    search               match              
+    toString             valueOf              
+    localeCompare        toUpperCase              
+    toLowerCase          indexOf              
+    lastIndexOf          replace              
+    trim                 length;              
+    charAt               charCodeAt              
+    at                   split              
+    repeat               slice              
+    substr               substring
+*/
+
+
+// match confusion 
+{
+    let x = 'Harshit puri puri'; 
+    let y = x.valueOf();
+    console.log(y)
+    // result is 1
+}
+
+
+// // search
+// {
+//     let x = 'Harshit'; 
+//     let y = x.search('a');
+//     console.log(y)
+//     // result is 1
+// }
+
+
+// // includes
+// {
+//     let x = 'Harshit'; 
+//     let y = x.includes('a');
+//     console.log(y)
+//     // true
+// }
+
+
+// // endsWith 
+// {
+//     let x = 'Harshit'; 
+//     let y = x.endsWith("r",3);
+//     console.log(y)
+//     // true
+// }
+
+// startsWith 
+// {
+//     let x = 'Harshit'; 
+//     let y = x.startsWith("a",1);
+//     console.log(y)
+//     // true
+// }
+
+// concat 
+// {
+//     let x = 'Harshit';
+//     let y = "puri";
+//     let z = x.concat(y);
+//     console.log(z)
+// }
---
+++ basic/array.js
@@ -0,0 +1,75 @@
+let arry = [1, 2, 3, 4, 5];
+let arryResult;
+
+// sort 
+// arryResult = arry.sort(); 
+// arryResult = arry.reverse();
+// arryResult = arry.pop();
+// arryResult = arry.push(3);
+// for(let a = 0; a<=10; a++){
+//     let prompt_value =  Number(prompt("enter your prompt"));
+//     arry.push(prompt_value);
+//     console.log(arry);
+// }
+
+// arryResult = arry.unshift(1);
+// arryResult = arry.join("-");
+// arryResult = arry.slice(1,2);
+// arryResult = arry.splice(3,0,3.3);
+// arryResult = Array.isArray(arry);
+// arryResult = arry.indexOf(1);
+// arryResult = arry.lastIndexOf(1); 
+// arryResult = arry.includes(4);
+
+// function some_method(ele){
+//     return ele <= 3;
+// }
+// arryResult = arry.some(some_method);
+
+// function every_method(ele){
+//     return ele <= 6;
+// }
+// arryResult = arry.every(every_method);
+
+// function find_method(ele){
+//     return ele == 3;
+// }
+// arryResult = arry.find(find_method);
+
+
+
+// function find_index_method(ele){
+//     return ele == 3;
+// }
+// arryResult = arry.findIndex(find_index_method);
+
+// function filter_function(ele){
+//     return ele <= 3;
+// }
+// arryResult = arry.filter(filter_function);
+
+
+// function map_method_fun(ele){
+//     return ele*2;
+// }
+// arryResult = arry.map(map_method_fun);
+
+// arry.forEach((ele)=>{
+//     console.log(ele);
+// })
+
+// arryResult = arry.toString();
+
+ 
+// arryResult = arry.fill("harshit");
+
+// entries
+
+arryResult = arry.entries();
+
+for(let x of arryResult){
+    console.log(x);
+}
+
+// console.log(arry);
+// console.log(arryResult);
