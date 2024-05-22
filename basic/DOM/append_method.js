45708f150c2fc54c5ab3623bab083b5d7e96dbe4
Harshit <hpurigoswami786@gmail.com>
2024-05-22 23:23:41 +0530

    insertAdjacent method create

--- basic/DOM/append_method.js
+++ basic/DOM/append_method.js
@@ -1,8 +1,30 @@
-let parentElement = document.querySelector('ul');
-let elementCreate  = document.createElement('li');
-let textCreate = document.createTextNode('hello Harshit');
-elementCreate.appendChild(textCreate);
-parentElement.appendChild(elementCreate);
+// let parentElement = document.querySelector('ul');
+// let elementCreate  = document.createElement('li');
+// let textCreate = document.createTextNode('hello Harshit');
+// elementCreate.appendChild(textCreate);
+// parentElement.appendChild(elementCreate);
+// parentElement.insertBefore(elementCreate,parentElement.children[0])
+// console.log(elementCreate);
 
-parentElement.insertBefore(elementCreate,parentElement.children[1])
-console.log(elementCreate)
+// Insert Adjacent method
+// insertAdjacentElement  /  insertAdjacentHTML
+// insertAdjacentText
+
+let parentElement = document.querySelector("ul");
+
+let newElement = document.createElement('li');
+let newText = document.createTextNode('Hy Harshit');
+newElement.appendChild(newText)
+
+// parentElement.insertAdjacentElement("beforebegin",newElement)
+// parentElement.insertAdjacentElement("afterbegin",newElement)
+// parentElement.insertAdjacentElement('beforeend',newElement)
+// parentElement.insertAdjacentElement('afterend',newElement)
+let full_html = "<li>Hy Harshit</li>";
+// parentElement.insertAdjacentHTML('afterbegin',full_html)
+// parentElement.insertAdjacentHTML('beforebegin',full_html)
+// parentElement.insertAdjacentHTML('beforeend',full_html)
+// parentElement.insertAdjacentHTML('afterend',full_html)
+// parentElement.insertAdjacentText('beforebegin','text')
+parentElement.insertAdjacentText('afterbegin',"dsfdfsdf")
+console.log(newElement)
